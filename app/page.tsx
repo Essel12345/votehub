ba4d5ade10ab2e666519cae3182b3b778804717b
Essel12345 <daudessel712@gmail.com>
2026-09-17 10:48:04 -0700

    fix: redirect homepage to login

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <main>
-      <h1>VoteHub Works!</h1>
-      <p>If you see this, Next.js is running</p>
-    </main>
-  );
+  redirect("/auth/login");
 }
